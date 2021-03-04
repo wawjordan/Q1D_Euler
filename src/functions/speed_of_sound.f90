732a00f4c2647d5e1b6ed3d735f02a0973834ce5
Will Jordan <wajordan@vt.edu>
2021-03-03 20:20:46 -0500

    Changed to function of pressure and density

--- src/functions/speed_of_sound.f90
+++ src/functions/speed_of_sound.f90
@@ -6,15 +6,15 @@
 module speed_of_sound_mod
 contains
 
-function speed_of_sound(gamma,R_gas,temperature)
+function speed_of_sound(gamma,pressure,rho)
 
   use set_precision, only : prec
   implicit none
 
-  real(prec), intent(in) :: gamma, R_gas, temperature
+  real(prec), intent(in) :: gamma, pressure, rho
   real(prec)             :: speed_of_sound
 
-  speed_of_sound = sqrt(gamma*R_gas*temperature)
+  speed_of_sound = sqrt(gamma*pressure/rho)
 
 end function speed_of_sound
 
