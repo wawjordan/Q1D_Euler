8cfa59499a8a63d6d1d4a6a6fbb8cf0db01c57da
Will Jordan <wajordan@vt.edu>
2021-03-05 18:00:59 -0500

    Added fluid_constants added spacing

--- src/functions/cons2prim.f90
+++ src/functions/cons2prim.f90
@@ -1,12 +1,13 @@
 ! converts conserved variables to primitve variables
 function cons2prim(U,V)
-  use set_precision, only : prec
-  use set_constants, only : one, half
-  use set_inputs,    only : gamma
+
+  use set_precision,      only : prec
+  use set_constants,      only : one, half
+  use fluid_constants,    only : gamma
 
   implicit none
 
-  real(prec), dimension(:,:), intent(in)  :: U
+  real(prec), dimension(:,:), intent(in)    :: U
   real(prec), dimension(:,:), intent(inout) :: V
 
   V(:,1) = U(:,1)
