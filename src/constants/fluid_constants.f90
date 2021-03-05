56ece1eccf1f493d9a32179ab7f6cfce8b8b7144
Will Jordan <wajordan@vt.edu>
2021-03-05 17:46:09 -0500

    initial commit --> further encapsulation of inputs and constants

---
+++ src/constants/fluid_constants.f90
@@ -0,0 +1,15 @@
+module fluid_constants
+  use set_precision, only : prec
+  use set_constants, only : zero, one
+  implicit none
+  private
+  public :: M_air, R_u, R_gas, gamma
+  real(prec) :: M_air = 28.96_prec
+  real(prec) :: R_u   = 8314.0_prec
+  real(prec) :: R_gas = zero
+  real(prec) :: gamma = 1.4_prec
+  contains
+  subroutine set_fluid_constants
+    R_gas = R_u/M_air
+  end suboutine set_fluid_constants
+end module fluid_constants
