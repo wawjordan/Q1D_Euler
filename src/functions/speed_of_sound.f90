361d4c4e346169f79f829fb9fb39ec577276d78d
Will Jordan <wajordan@vt.edu>
2021-03-06 20:54:28 -0500

    removed intent(in) modified spacing

--- src/functions/speed_of_sound.f90
+++ src/functions/speed_of_sound.f90
@@ -10,9 +10,9 @@ function speed_of_sound(pressure,rho)
 
   implicit none
 
-  real(prec), intent(in) :: pressure, rho
-  real(prec)             :: speed_of_sound
-
+  real(prec) :: pressure, rho
+  real(prec) :: speed_of_sound
+  
   speed_of_sound = sqrt(gamma*pressure/rho)
 
 end function speed_of_sound
