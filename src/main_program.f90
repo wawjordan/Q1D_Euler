25da2309d4c3f3b77f06d39af7f463415fc2343b
Will Jordan <wajordan@vt.edu>
2021-03-07 15:56:16 -0500

    changed to use variable_conversion module

--- src/main_program.f90
+++ src/main_program.f90
@@ -4,13 +4,14 @@ program main_program
   use set_constants, only : set_derived_constants
   use fluid_constants, only : set_fluid_constants
   use set_inputs, only : set_derived_inputs
+  use variable_conversion
   use geometry, only : setup_geometry, teardown_geometry
   use init_problem, only : initialize
   use grid_type
   use soln_type
   implicit none
   
-  !character(len=100) :: header_str
+  character(len=100) :: header_str
   integer :: i
   type( grid_t ) :: grid
   type( soln_t ) :: soln
@@ -21,21 +22,20 @@ program main_program
   call setup_geometry(grid,soln)
   
   call initialize(grid,soln)
-  do i = 1, size(grid%xc)
-    write(*,*) grid%xc(i), grid%Ac(i), soln%M(i)
-  enddo
+  !do i = 1, size(grid%xc)
+  !  write(*,*) grid%xc(i), grid%Ac(i), soln%M(i), soln%T(i)
+  !enddo
   !call allocate_exact_q1d( ex_soln )
   
   !call solve_exact_q1d(ex_soln)
   !call isentropic_relations(ex_soln%M(1), ex_soln%V(1,1), ex_soln%T(1))
-  !write(header_str,*) '|    x   |    A    |         M         |'// &
-  !&  '        rho        |         u        |         p        |'
-  !100 format(2(F9.4),4(F20.14))
-  !write(*,*) trim(adjustl(header_str))
-  !do i = 1,imax
-  !  write(*,100) xq(i), Aq(i), ex_soln%M(i), ex_soln%V(i,1), ex_soln%V(i,2), ex_soln%V(i,3)
-  !end do
-  !write(*,*) 'T = ', ex_soln%T
+  write(header_str,*) '|    x   |    A    |         M         |'// &
+  &  '        rho        |         u        |         p        |'
+  100 format(2(F9.4),4(F20.14))
+  write(*,*) trim(adjustl(header_str))
+  do i = 1,size(grid%xc)
+    write(*,100) grid%xc(i), grid%Ac(i), soln%M(i), soln%V(i,1), soln%V(i,2), soln%V(i,3)/1000.0_prec
+  end do
   !call deallocate_exact_q1d( ex_soln )
   
   call teardown_geometry(grid,soln)
