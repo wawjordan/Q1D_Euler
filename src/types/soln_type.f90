207712e87c4450a394abba2d7304eeb904598575
Will Jordan <wajordan@vt.edu>
2021-03-07 14:48:44 -0500

    fixed array sizes for M, T, dt, and lambda

--- src/types/soln_type.f90
+++ src/types/soln_type.f90
@@ -11,12 +11,12 @@ module soln_type
     real(prec), allocatable, dimension(:,:) :: U
     real(prec), allocatable, dimension(:,:) :: F
     real(prec), allocatable, dimension(:,:) :: D
-    real(prec), allocatable, dimension(:,:) :: M
-    real(prec), allocatable, dimension(:,:) :: T
+    real(prec), allocatable, dimension(:) :: M
+    real(prec), allocatable, dimension(:) :: T
     real(prec), allocatable, dimension(:,:) :: R
-    real(prec), allocatable, dimension(:,:) :: dt
+    real(prec), allocatable, dimension(:) :: dt
     real(prec), allocatable, dimension(:,:) :: eps
-    real(prec), allocatable, dimension(:,:) :: lambda
+    real(prec), allocatable, dimension(:) :: lambda
 
   end type soln_t
 
@@ -39,12 +39,12 @@ contains
              soln%V(i_low:i_high,neq),      &
              soln%F(i_low:i_high,neq),      &
              soln%D(i_low:i_high,neq),      &
-             soln%M(i_low:i_high,neq),      &
-             soln%T(i_low:i_high,neq),      &
+             soln%M(i_low:i_high),      &
+             soln%T(i_low:i_high),      &
              soln%R(i_low:i_high,neq),      &
-             soln%dt(i_low:i_high,neq),     &
+             soln%dt(i_low:i_high),     &
              soln%eps(i_low:i_high,neq),    &
-             soln%lambda(i_low:i_high,neq)  )
+             soln%lambda(i_low:i_high)  )
 
     soln%U   = zero
     soln%V   = zero
