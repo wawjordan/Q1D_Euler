a23e99344d4c55d4e525fcfc7a4ed2eda0bf2131
Will Jordan <wajordan@vt.edu>
2021-03-07 11:09:50 -0500

    finished writing subroutine

--- src/functions/calc_time_step.f90
+++ src/functions/calc_time_step.f90
@@ -1,5 +1,17 @@
-subroutine calc_time_step( lambda, V )
+subroutine calc_time_step( lambda, dt,  V )
   
   use set_precision, only : prec
   use fluid_constants, only : gamma
   use set_inputs, only : CFL
+  
+  implicit none
+  
+  real(prec), dimension(:), intent(inout)   :: lambda
+  real(prec), dimension(:), intent(inout)   :: dt
+  real(prec), dimension(:,:), intent(inout) :: V
+  real(prec), external :: speed_of_sound
+  
+  lambda(:) = abs(V(:,2)) + speed_of_sound(V(:,3),V(:,1))
+  dt(:)     = CFL*dx/lambda(:)
+
+end subroutine calc_time_step
