86f76f48ccbe3d14769223d3eea08ca1284092f2
Will Jordan <wajordan@vt.edu>
2021-03-08 14:15:00 -0500

    added a second layer of ghost cells

--- src/boundary/basic_boundaries.f90
+++ src/boundary/basic_boundaries.f90
@@ -12,13 +12,16 @@ contains
   subroutine sub_in_bndry( M, U1, V1 )
     
     real(prec), dimension(:), intent(in) :: M
-    real(prec), dimension(1,neq), intent(out) :: U1, V1
-    real(prec), dimension(1)  :: M0, T1
+    real(prec), dimension(2,neq), intent(out) :: U1, V1
+    real(prec), dimension(2)  :: M0, T1
     T1 = one
-    M0 = two*M(1) - M(2)
-    
-    if (M0<eps) then
-      M0 = eps
+    M0(1) = two*M(1) - M(2)
+    if (M0(1)<eps) then
+      M0(1) = eps
+    end if
+    M0(2) = two*M0(1) - M(2)
+    if (M0(2)<eps) then
+      M0(2) = eps
     end if
     
     call isentropic_relations(M0,V1,T1)
@@ -31,7 +34,7 @@ contains
     
     use set_inputs, only : pb
     real(prec), dimension(:,:), intent(in) :: V
-    real(prec), dimension(1,neq), intent(out) :: Un, Vn
+    real(prec), dimension(2,neq), intent(out) :: Un, Vn
     integer :: i
         
     i = size(V,1)
@@ -39,6 +42,10 @@ contains
     Vn(1,1) = two*V(i,1) - V(i-1,1)
     Vn(1,2) = two*V(i,2) - V(i-1,2)
     Vn(1,3) = two*pb - V(i,3)
+
+    Vn(2,1) = two*Vn(1,1) - V(i,1)
+    Vn(2,2) = two*Vn(1,2) - V(i,2)
+    Vn(2,3) = two*Vn(1,3) - pb
     
     call prim2cons(Un,Vn)
     
@@ -48,12 +55,13 @@ contains
   subroutine sup_out_bndry( V, Un, Vn )
     
     real(prec), dimension(:,:), intent(in) :: V
-    real(prec), dimension(1,:), intent(out) :: Un, Vn
+    real(prec), dimension(2,:), intent(out) :: Un, Vn
     integer :: i
      
     i = size(V,1)
     
-    Vn = two*V(i,:) - V(i-1,:)
+    Vn(1,:) = two*V(i,:) - V(i-1,:)
+    Vn(2,:) = two*Vn(1,:) - V(i,:)
     
     call prim2cons(Un,Vn)
     
