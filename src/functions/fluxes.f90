00f8aa912021bd3a4557ec500b137c20340842f9
Will Jordan <wajordan@vt.edu>
2021-03-07 19:54:42 -0500

    Added an extra set of parentheses (didn't end up being necessary)

--- src/functions/fluxes.f90
+++ src/functions/fluxes.f90
@@ -11,10 +11,10 @@ contains
 subroutine central_flux(U,F)
   
   real(prec), dimension(:,:), intent(inout)  :: U
-  real(prec), dimension(:,:), intent(in)     :: F
+  real(prec), dimension(:,:), intent(inout)  :: F
   
   F(:,1) = U(:,2)
-  F(:,2) = half*(three-gamma)*U(:,2)**2/U(:,1) + (gamma-one)*U(:,3)
+  F(:,2) = half*(three-gamma)*( U(:,2)**2 )/U(:,1) + (gamma-one)*U(:,3)
   F(:,3) = U(:,3)*U(:,2)/U(:,1) + U(:,2)/U(:,1)*( (gamma-one)*U(:,3) - half*(gamma-one)*U(:,2)**2/U(:,1) )
   
 end subroutine central_flux
