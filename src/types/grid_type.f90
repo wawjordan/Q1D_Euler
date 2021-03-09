9d35a03b1b36bac8ea4c3377aff6225b553707b7
Will Jordan <wajordan@vt.edu>
2021-03-09 18:37:20 -0500

    fixed bounds

--- src/types/grid_type.f90
+++ src/types/grid_type.f90
@@ -25,24 +25,24 @@ contains
   subroutine allocate_grid( grid )
     
     use set_constants, only : zero, one, half
-    use set_inputs   , only : imax, xmin, xmax, n_ghost_cells
+    use set_inputs   , only : imax, xmin, xmax, ig_low, ig_high
     
     type( grid_t ), intent( inout ) :: grid
-    integer :: i, i_low, i_high
+    integer :: i
     
-    i_low  = 1 - n_ghost_cells
-    i_high = imax + n_ghost_cells
+    !i_low  = 1 - n_ghost_cells
+    !i_high = imax + n_ghost_cells
     
     grid%dx = (xmax - xmin)/real(imax,prec)
 
-    allocate( grid%xi(i_low-1:i_high), &
-              grid%xc(i_low:i_high)  , &
-              grid%Ai(i_low-1:i_high), &
-              grid%Ac(i_low:i_high)  , &
-              grid%dAc(i_low:i_high)   )
+    allocate( grid%xi(ig_low-1:ig_high), &
+              grid%xc(ig_low:ig_high)  , &
+              grid%Ai(ig_low-1:ig_high), &
+              grid%Ac(ig_low:ig_high)  , &
+              grid%dAc(ig_low:ig_high)   )
     
-    grid%xi = [ (xmin + real(i,prec)/real(imax,prec)*(xmax-xmin),i=i_low-1,i_high) ]
-    grid%xc = [ (half*(grid%xi(i) + grid%xi(i+1)),i=i_low-1,i_high-1) ]
+    grid%xi = [ (xmin + real(i,prec)/real(imax,prec)*(xmax-xmin),i=ig_low-1,ig_high) ]
+    grid%xc = [ (half*(grid%xi(i) + grid%xi(i+1)),i=ig_low-1,ig_high-1) ]
     grid%Ai = one
     grid%Ac = one
     grid%dAc =  zero
