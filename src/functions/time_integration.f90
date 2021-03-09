ee807f63b7bd579b8f1c3c0b82db77cbe380542f
Will Jordan <wajordan@vt.edu>
2021-03-08 19:21:48 -0500

    Changed calc_time_step_routine to take grid type input Fixed rank mismatch errors in explicit_euler and residual_norms Commented out advance solution

--- src/functions/time_integration.f90
+++ src/functions/time_integration.f90
@@ -11,38 +11,44 @@ module time_integration
   
 contains
 
-subroutine calc_time_step( lambda, dt,  V )
+subroutine calc_time_step( grid, V, lambda, dt )
 
   use fluid_constants,     only : gamma
-  use set_inputs,          only : CFL, dx
+  use set_inputs,          only : CFL
 
-  real(prec), dimension(:),   intent(inout) :: lambda
-  real(prec), dimension(:),   intent(inout) :: dt
-  real(prec), dimension(:,:), intent(inout) :: V
-  real(prec), dimension(size(lambda))       :: sound_speed
+  real(prec), dimension(:,:), intent(in)  :: V
+  type(grid_t),               intent(in)  :: grid
+  real(prec), dimension(:),   intent(out) :: lambda
+  real(prec), dimension(:),   intent(out) :: dt
+  
+  real(prec), dimension(size(lambda))     :: sound_speed
 
   call speed_of_sound(V(:,3),V(:,1),sound_speed)
 
   lambda(:) = abs(V(:,2)) + sound_speed
-  dt(:)     = CFL*dx/lambda(:)
+  dt(:)     = CFL*grid%dx/lambda(:)
 
 end subroutine calc_time_step
 
  
 subroutine explicit_euler(grid,S,dt,F,U,R)
   
-  type(grid_t), intent(in) :: grid
+  type(grid_t),               intent(in)    :: grid
   real(prec), dimension(:,:), intent(inout) :: U
-  real(prec), dimension(:,:), intent(in) :: F
-  real(prec), dimension(:), intent(in) :: S,dt
-  real(prec), dimension(:,:), intent(out) :: R
+  real(prec), dimension(:,:), intent(in)    :: F
+  real(prec), dimension(:)  , intent(in)    :: S,dt
+  real(prec), dimension(:,:), intent(out)   :: R
   integer :: i
   
   i = size(F,1)
-  R = F(2:i+1,:)*grid%Ai(2:i+1) - F(1:i,:)*grid%Ai(1:i)
-  R(:,2) = R(:,2) - S*grid%x
   
-  U = U + dt/(grid%Ac*grid%dx)*R
+  R(:,1) = F(2:i+1,1)*grid%Ai(2:i+1) - F(1:i,1)*grid%Ai(1:i)
+  R(:,2) = F(2:i+1,2)*grid%Ai(2:i+1) - F(1:i,2)*grid%Ai(1:i) - S*grid%xc
+  R(:,3) = F(2:i+1,3)*grid%Ai(2:i+1) - F(1:i,3)*grid%Ai(1:i)
+  
+  U(:,1) = U(:,1) + dt/(grid%Ac*grid%dx)*R(:,1)
+  U(:,2) = U(:,2) + dt/(grid%Ac*grid%dx)*R(:,2)
+  U(:,3) = U(:,3) + dt/(grid%Ac*grid%dx)*R(:,3)
   
 end subroutine explicit_euler
 
@@ -53,44 +59,44 @@ subroutine residual_norms(R,Rnorm,pnorm)
   integer :: pnorm
   
   if (pnorm == 0) then
-    Rnorm = abs(maxval(R),1)
+    Rnorm(1,:) = abs(maxval(R,1))
   elseif (pnorm == 1) then
-    Rnorm = (one/real(size(R,1)))*sum(abs(R),1)
+    Rnorm(1,:) = (one/real(size(R,1)))*sum(abs(R),1)
   elseif (pnorm == 2) then 
-    Rnorm = sqrt((one/real(size(R,1)))*sum(R**2,1))
+    Rnorm(1,:) = sqrt((one/real(size(R,1)))*sum(R**2,1))
   else 
-    Rnorm = abs(maxval(R),1)
+    Rnorm(1,:) = abs(maxval(R,1))
   end if
   
 end subroutine residual_norms
 
-subroutine advance_solution(grid,soln)
-  
-  type(grid_t) :: grid
-  type(soln_t) :: soln
-  
-  call calculate_time_step(soln%lambda,soln%dt,soln%V)
-
-  call enforce_bndry(soln)
-
-  call extrapolate_variables(soln%U,Uextrap,...)
-
-  call central_flux(Uextrap,Fextrap)
-
-  call jst_damping(soln%lambda,soln%D,soln%E,Fextrap,...)
-
-  call calculate_sources(soln%S,...)
-
-  call explicit_euler(grid,soln%S,soln%dt,Fextrap,soln%U,soln%R)
-
-  call cons2prim(soln%U,soln%V)
-
-  call isentropic_relations(soln%M,soln%V,soln%T) ! calc_mach_number?
-
-  call output_soln()
-
-  call enforce_bndry(soln)
-
-end subroutine advance_solution
+!subroutine advance_solution(grid,soln)
+!  
+!  type(grid_t) :: grid
+!  type(soln_t) :: soln
+!  
+!  call calculate_time_step(soln%lambda,soln%dt,soln%V)
+!
+!  call enforce_bndry(soln)
+!
+!  call extrapolate_variables(soln%U,Uextrap,...)
+!
+!  call central_flux(Uextrap,Fextrap)
+!
+!  call jst_damping(soln%lambda,soln%D,soln%E,Fextrap,...)
+!
+!  call calculate_sources(soln%S,...)
+!
+!  call explicit_euler(grid,soln%S,soln%dt,Fextrap,soln%U,soln%R)
+!
+!  call cons2prim(soln%U,soln%V)
+!
+!  call isentropic_relations(soln%M,soln%V,soln%T) ! calc_mach_number?
+!
+!  call output_soln()
+!
+!  call enforce_bndry(soln)
+!
+!end subroutine advance_solution
 
 end module time_integration
