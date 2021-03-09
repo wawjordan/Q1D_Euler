ad6b126519579dfee4bbd0bdb3e11df581258b26
Will Jordan <wajordan@vt.edu>
2021-03-09 17:53:21 -0500

    Added ig_low and ig_high (ghost cell start/stop indices) to use section Added write statements to check bounds during runtime

--- src/init_problem.f90
+++ src/init_problem.f90
@@ -3,7 +3,7 @@ module init_problem
   use set_precision,   only : prec
   use set_constants,   only : zero, one, two, half
   use fluid_constants, only : R_gas, gamma
-  use set_inputs,      only : p0, T0, eps
+  use set_inputs,      only : p0, T0, eps, ig_low, ig_high
   use soln_type
   use grid_type
   use variable_conversion
@@ -21,18 +21,21 @@ module init_problem
     implicit none
     
     integer :: i
-    
     type( soln_t ), intent(inout) :: soln
     type( grid_t ), intent(inout) :: grid
     
     soln%M(:) = 0.9_prec*grid%xc(:) + one
-    do i = lbound(grid%xc,1),ubound(grid%xc,1)
+    do i = ig_low,ig_high
       if ( soln%M(i) < eps ) then
         soln%M(i) = eps
       end if
     end do
     call isentropic_relations(soln%M,soln%V)
     call prim2cons(soln%U,soln%V)
+    write(*,*) 'grid%xc:  ','low = ',lbound(grid%xc,1),'  high= ',ubound(grid%xc,1)
+    write(*,*) 'grid%xi:  ','low = ',lbound(grid%xi,1),'  high= ',ubound(grid%xi,1)
+    write(*,*) 'soln%U:  ','low = ',lbound(soln%U,1),'  high= ',ubound(soln%U,1)
+
   end subroutine initialize
 
 end module init_problem
