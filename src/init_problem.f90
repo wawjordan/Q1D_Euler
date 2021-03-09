9850400d7783eee77afc4b4193b682c84380ddbc
Will Jordan <wajordan@vt.edu>
2021-03-08 19:26:51 -0500

    Added check for small/negative mach numbers

--- src/init_problem.f90
+++ src/init_problem.f90
@@ -3,7 +3,7 @@ module init_problem
   use set_precision,   only : prec
   use set_constants,   only : zero, one, two, half
   use fluid_constants, only : R_gas, gamma
-  use set_inputs,      only : p0, T0
+  use set_inputs,      only : p0, T0, eps
   use soln_type
   use grid_type
   use variable_conversion
@@ -26,6 +26,11 @@ module init_problem
     type( grid_t ), intent(inout) :: grid
     
     soln%M(:) = 0.9_prec*grid%xc(:) + one
+    do i = lbound(grid%xc,1),ubound(grid%xc,1)
+      if ( soln%M(i) < eps ) then
+        soln%M(i) = eps
+      end if
+    end do
     call isentropic_relations(soln%M,soln%V,soln%T)
     
   end subroutine initialize
