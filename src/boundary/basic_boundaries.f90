87b53c2d8edcbad3e13e2a64598f7a28698b03a8
Will Jordan <wajordan@vt.edu>
2021-03-08 20:33:45 -0500

    modified for use with ghost cells

--- src/boundary/basic_boundaries.f90
+++ src/boundary/basic_boundaries.f90
@@ -2,68 +2,77 @@ module basic_boundaries
 
 use set_precision, only : prec
 use set_constants, only : one, two
-use set_inputs,    only : neq, eps
+use set_inputs,    only : imax, neq, eps
 use variable_conversion, only : prim2cons, isentropic_relations
 
 implicit none
 
 contains
 
-  subroutine sub_in_bndry( M, U1, V1 )
+  subroutine sub_in_bndry( M, U, V )
     
-    real(prec), dimension(:), intent(in) :: M
-    real(prec), dimension(2,neq), intent(out) :: U1, V1
-    real(prec), dimension(2)  :: M0, T1
-    T1 = one
-    M0(1) = two*M(1) - M(2)
-    if (M0(1)<eps) then
-      M0(1) = eps
-    end if
-    M0(2) = two*M0(1) - M(2)
-    if (M0(2)<eps) then
-      M0(2) = eps
-    end if
+    real(prec), dimension(:),   intent(inout) :: M
+    real(prec), dimension(:,:), intent(inout) :: U, V
+    real(prec), dimension(:)  :: T
+    integer :: i, i_low
     
-    call isentropic_relations(M0,V1,T1)
-    call prim2cons(U1,V1)
+    i_low  = lbound(M,1)
+    
+    if ( i_low < 1 ) then
+      do i = 0,-1,i_low
+        M(i) = 2*M(i+1) - M(i+2)
+      end do
+      call isentropic_relations(M(i_low:0),V(i_low:0,:),T)
+      call prim2cons(U(i_low:0,:),V(i_low:0))
+    end if
     
   end subroutine sub_in_bndry
 
 
-  subroutine sub_out_bndry( V, Un, Vn )
+  subroutine sub_out_bndry( U, V )
     
     use set_inputs, only : pb
-    real(prec), dimension(:,:), intent(in) :: V
-    real(prec), dimension(2,neq), intent(out) :: Un, Vn
-    integer :: i
-        
-    i = size(V,1)
+    real(prec), dimension(:,:), intent(inout) :: U, V
+    integer :: i, i_high
     
-    Vn(1,1) = two*V(i,1) - V(i-1,1)
-    Vn(1,2) = two*V(i,2) - V(i-1,2)
-    Vn(1,3) = two*pb - V(i,3)
-
-    Vn(2,1) = two*Vn(1,1) - V(i,1)
-    Vn(2,2) = two*Vn(1,2) - V(i,2)
-    Vn(2,3) = two*Vn(1,3) - pb
+    i_high = ubound(V,1)
     
-    call prim2cons(Un,Vn)
+    if ( i_high > imax ) then
+      do i = imax+1,i_high
+        if (i == imax+1) then
+          V(i,1) = two*V(i-1,1) - V(i-2,1)
+          V(i,2) = two*V(i-1,2) - V(i-2,2)
+          V(i,3) = two*pb - V(i-1,3)
+        else
+          V(i,1) = V(i-1,1)
+          V(i,2) = V(i-1,2)
+          V(i,3) = V(i-1,3)
+        end if
+      end do
+      call prim2cons(U(imax+1:i_high,:),V(imax+1:i_high,:))
+    end if
     
-  end subroutine sub_in_bndry
+  end subroutine sub_out_bndry
   
   
-  subroutine sup_out_bndry( V, Un, Vn )
+  subroutine sup_out_bndry( U, V )
     
-    real(prec), dimension(:,:), intent(in) :: V
-    real(prec), dimension(2,:), intent(out) :: Un, Vn
-    integer :: i
-     
-    i = size(V,1)
+    real(prec), dimension(:,:), intent(inout) :: U, V
+    integer :: i, j, i_high
     
-    Vn(1,:) = two*V(i,:) - V(i-1,:)
-    Vn(2,:) = two*Vn(1,:) - V(i,:)
+    i_high = ubound(V,1)
     
-    call prim2cons(Un,Vn)
+    if ( i_high > imax ) then
+      do i = imax+1,i_high
+        do j = 1,neq
+          V(i,j) = two*V(i-1,j) - V(i-2,j)
+          if ( V(i,j) < eps) then
+            V(i,j) = eps
+          end if
+        end do
+      end do
+      call prim2cons(U(imax+1:i_high,:),V(imax+1:i_high,:))
+    end if
     
   end subroutine sub_in_bndry
   
