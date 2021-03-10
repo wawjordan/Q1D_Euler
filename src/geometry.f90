74dfa7df930d50094a7be778a11339fed453a1d9
Will Jordan <wajordan@vt.edu>
2021-03-10 15:37:20 -0500

    Added comment blocks. Neatened up indentation

--- src/geometry.f90
+++ src/geometry.f90
@@ -13,14 +13,24 @@ module geometry
   
   public :: setup_geometry, teardown_geometry
   
-
-
   contains
   
+  !============================= setup geometry  =============================80
+  !>
+  !! Description: 
+  !!
+  !! Inputs:      grid : 
+  !!              soln : 
+  !!
+  !! Outputs:     grid :
+  !!              soln :
+  !<
+  !===========================================================================80
   subroutine setup_geometry( grid, soln )
     
     type( soln_t ), intent(inout) :: soln
     type( grid_t ), intent(inout) :: grid
+    
     integer :: i, i_low, i_high
     
     call allocate_grid( grid )
@@ -30,11 +40,12 @@ module geometry
     i_high = ubound(grid%xi,1)
     
     grid%Ai(i_low:0) = area( grid%xi(0) )
+    
     do i = 1,imax
       grid%Ai(i) = area( grid%xi(i) )
     end do
-    grid%Ai(imax:i_high) = area( grid%xi(imax) )
     
+    grid%Ai(imax:i_high) = area( grid%xi(imax) )
     
     i_low  = lbound(grid%xc,1)
     i_high = ubound(grid%xc,1)
@@ -46,14 +57,25 @@ module geometry
     grid%Ac(imax:i_high) = area( grid%xc(imax) )
     
     
-    grid%dAc(i_low:1) = darea(grid%xc(1))
+    grid%dAc(i_low:1) = darea( grid%xc(1) )
     do i = 2,imax-1
       grid%dAc(i) = darea( grid%xc(i) )
     end do
-    grid%dAc(imax:i_high) = darea (grid%xc(imax) )
+    grid%dAc(imax:i_high) = darea( grid%xc(imax) )
     
   end subroutine setup_geometry
   
+  !========================== teardown geometry  =============================80
+  !>
+  !! Description: 
+  !!
+  !! Inputs:      grid : 
+  !!              soln : 
+  !!
+  !! Outputs:     grid :
+  !!              soln :
+  !<
+  !===========================================================================80
   subroutine teardown_geometry( grid, soln )
     
     type( soln_t ) :: soln
