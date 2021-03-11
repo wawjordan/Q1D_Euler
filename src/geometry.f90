9487fafbeb34d4e2d6935542964efea7fb7984de
Will Jordan <wajordan@vt.edu>
2021-03-11 14:19:40 -0500

    Restored stashed changes

--- src/geometry.f90
+++ src/geometry.f90
@@ -3,7 +3,7 @@ module geometry
   use set_precision,   only : prec
   use set_constants,   only : zero, one, two, half, pi
   use fluid_constants, only : R_gas, gamma
-  use set_inputs,      only : imax, area, darea, eps
+  use set_inputs,      only : area, darea, eps
   use soln_type
   use grid_type
 
@@ -28,40 +28,27 @@ module geometry
   !===========================================================================80
   subroutine setup_geometry( grid, soln )
     
+    use set_inputs, only : imax, i_low, i_high
+    
     type( soln_t ), intent(inout) :: soln
     type( grid_t ), intent(inout) :: grid
     
-    integer :: i, i_low, i_high
+    integer :: i
     
     call allocate_grid( grid )
     call allocate_soln( soln )
     
-    i_low  = lbound(grid%xi,1)
-    i_high = ubound(grid%xi,1)
-    
-    grid%Ai(i_low:0) = area( grid%xi(0) )
-    
-    do i = 1,imax
+    do i = i_low-1,i_high
       grid%Ai(i) = area( grid%xi(i) )
     end do
     
-    grid%Ai(imax:i_high) = area( grid%xi(imax) )
-    
-    i_low  = lbound(grid%xc,1)
-    i_high = ubound(grid%xc,1)
-    
-    grid%Ac(i_low:1) = area( grid%xc(1) )
-    do i = 2,imax-1
+    do i = i_low,i_high
       grid%Ac(i) = area( grid%xc(i) )
     end do
-    grid%Ac(imax:i_high) = area( grid%xc(imax) )
     
-    
-    grid%dAc(i_low:1) = darea( grid%xc(1) )
-    do i = 2,imax-1
+    do i = i_low,i_high
       grid%dAc(i) = darea( grid%xc(i) )
     end do
-    grid%dAc(imax:i_high) = darea( grid%xc(imax) )
     !grid%dAc(i_low:1) = darea( grid%xc(1) )
     !do i = 1,imax-1
     !  grid%dAc(i) = ( grid%Ac(i+1) - grid%Ac(i) )/grid%dx
