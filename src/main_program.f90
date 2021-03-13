7805066713d391140c5ff735fe4f35e4a23e7212
Will Jordan <wajordan@vt.edu>
2021-03-12 21:30:12 -0500

    Added some additional write out statements. Applied BCs prior to calculating flux - seemed to yield slightly less divergent results.

--- src/main_program.f90
+++ src/main_program.f90
@@ -21,6 +21,7 @@ program main_program
   character(len=100) :: header_str2
   character(len=100) :: header_str3
   character(len=100) :: header_str4
+  character(len=100) :: header_str5
   integer :: i, j
   type( grid_t )      :: grid
   type( soln_t )      :: soln
@@ -47,8 +48,9 @@ program main_program
   write(header_str3,*) '|    x   |        F(1)       |'// &
   & '       F(2)       |       F(3)       |'
   write(header_str4,*) '|    x   |    dA   |         S         |'// &
-  & '         dt       |       lambda     |'
- 
+  & '         dt       |       lambda     |' 
+  write(header_str5,*) '|    x   |        D(1)       |'// &
+  & '       D(2)       |       D(3)       |'
 
   write(*,*) 'Initial Conditions: Primitives'
   write(*,*) trim(adjustl(header_str1))
@@ -84,7 +86,7 @@ program main_program
   
   
   call cons2prim( soln%U, soln%V )
-  
+  call isentropic_relations(soln%mach,soln%V)
   write(*,*) 'cons2prim Applied: Primitives'
   write(*,*) trim(adjustl(header_str1))
   do i = ig_low,ig_high
@@ -98,9 +100,8 @@ program main_program
   end do 
   write(*,*)
  
-  call calculate_sources(soln%V,grid%dAc,soln%src)
-
-  call calc_time_step(grid%dx,soln%V,soln%asnd,soln%lambda,soln%dt)
+  !call calculate_sources(soln%V,grid%dAc,soln%src)
+  !call calc_time_step(grid%dx,soln%V,soln%asnd,soln%lambda,soln%dt)
   write(*,*) 'Calculate Sources and Time step:'
   write(*,*) trim(adjustl(header_str4))
   do i = ig_low,ig_high
@@ -111,19 +112,36 @@ program main_program
     end if
   end do
   write(*,*)
+ ! call prim2cons(soln%U,soln%V)
+ ! call central_flux( soln%U, soln%F )
+  write(*,*) 'central_flux Applied:'
+  write(*,*) trim(adjustl(header_str3))
+  do i = i_low-1,i_high
+    write(*,200) grid%xi(i), soln%F(i,1), soln%F(i,2), soln%F(i,3)
+  end do
 
 
+!  call jst_damping(soln%lambda,soln%U,soln%V,soln%D) 
+  write(*,*) 'JST Damping Applied:'
+  write(*,*) trim(adjustl(header_str5))
+  do i = i_low-1,i_high
+    write(*,200) grid%xi(i), soln%D(i,1), soln%D(i,2), soln%D(i,3)
+  end do
 
-  do j = 1,200
+  do j = 1,800
     
     call calculate_sources(soln%V,grid%dAc,soln%src)
     
     call calc_time_step(grid%dx,soln%V,soln%asnd,soln%lambda,soln%dt)
   
-   ! call cons2prim( soln%U, soln%V )
-  
+    
+    call sub_in_bndry( soln%mach, soln%U, soln%V )
+    
+    call sup_out_bndry( soln%U, soln%V )
+    call cons2prim( soln%U, soln%V )
+    
     call central_flux(soln%U, soln%F)
-  
+    call prim2cons(soln%U,soln%V)
     call jst_damping(soln%lambda,soln%U,soln%V,soln%D)
   
     soln%F = soln%F + soln%D
@@ -132,11 +150,11 @@ program main_program
     
     call update_mach(soln%V,soln%mach )
     
-    call sub_in_bndry( soln%mach, soln%U, soln%V )
+    !call sub_in_bndry( soln%mach, soln%U, soln%V )
     
-    call sup_out_bndry( soln%U, soln%V )
+    !call sup_out_bndry( soln%U, soln%V )
    
-   call cons2prim(soln%U,soln%V)
+    call cons2prim(soln%U,soln%V)
 ! 100 format(2(F9.4),4(F20.14))
 ! write(*,*) 'Initial solution values at cell centers:'
 ! write(header_str,*) '|    x   |    A    |         M         |'// &
@@ -145,9 +163,9 @@ program main_program
 ! do i = ig_low,ig_high
 !   write(*,100) grid%xc(i), grid%Ac(i), soln%M(i), soln%V(i,1), soln%V(i,2), soln%V(i,3)/1000.0_prec
  !end do
- 
+  if (mod(j,4)==0) then
   call output_soln(grid,soln,j)
- 
+  end if
   end do
   !call prim2cons(soln%U,soln%V)
   !write(*,*) 'soln%U:  ','low = ',lbound(soln%U,1),'  high= ',ubound(soln%U,1)
