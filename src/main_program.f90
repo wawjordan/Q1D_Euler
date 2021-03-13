f59c75b2fb5e4d5c6be31bf2c55adc065928122f
Will Jordan <wajordan@vt.edu>
2021-03-13 14:28:55 -0500

    Added residual_norm calculation and output Added stopping criterion

--- src/main_program.f90
+++ src/main_program.f90
@@ -1,10 +1,10 @@
 program main_program
   
- ! use set_precision, only : prec
+  use set_precision, only : prec
   use set_constants, only : set_derived_constants
   use fluid_constants, only : set_fluid_constants
-  use set_inputs, only : set_derived_inputs, imax
-  use set_inputs, only : i_high, i_low, ig_high, ig_low
+  use set_inputs, only : set_derived_inputs, max_iter, neq, tol
+  !use set_inputs, only : i_high, i_low, ig_high, ig_low
   use variable_conversion
   use time_integration
   use basic_boundaries
@@ -18,15 +18,20 @@ program main_program
   implicit none
   
   character(len=100) :: header_str1
-  character(len=100) :: header_str2
-  character(len=100) :: header_str3
-  character(len=100) :: header_str4
-  character(len=100) :: header_str5
-  integer :: i, j
+  !character(len=100) :: header_str2
+  !character(len=100) :: header_str3
+  !character(len=100) :: header_str4
+  !character(len=100) :: header_str5
+  integer :: j
+  real(prec), dimension(1,3) :: rnorm
   type( grid_t )      :: grid
   type( soln_t )      :: soln
   type( exact_q1d_t ) :: ex_soln  
-
+  
+  100 format(1(I0.8),3(G20.7))
+  
+  write(header_str1,*) " Iter  |   ||density||    |"// &
+  & "   ||velocity||    |   ||pressure||    |"
   call set_derived_constants
   call set_fluid_constants
   call set_derived_inputs
@@ -34,30 +39,9 @@ program main_program
   
   call initialize(grid,soln)
   
-  !call allocate_exact_q1d( ex_soln, grid )
+  call allocate_exact_q1d( ex_soln, grid )
   
-  !call solve_exact_q1d( ex_soln, grid)
-  
-!  100 format(2(F9.4),4(G20.7))
-!  200 format(1(F9.4),3(G20.7))
-!  300 format(2(F9.4),3(G20.7))
-!  write(header_str1,*) '|    x   |    A    |         M         |'// &
-!  &  '        rho        |         u        |         p        |'
-!  write(header_str2,*) '|    x   |        U(1)       |'// &
-!  & '       U(2)       |       U(3)       |'
-!  write(header_str3,*) '|    x   |        F(1)       |'// &
-!  & '       F(2)       |       F(3)       |'
-!  write(header_str4,*) '|    x   |    dA   |         S         |'// &
-!  & '         dt       |       lambda     |' 
-!  write(header_str5,*) '|    x   |        D(1)       |'// &
-!  & '       D(2)       |       D(3)       |'
-
-!  write(*,*) 'Initial Conditions: Primitives'
-!  write(*,*) trim(adjustl(header_str1))
-!  do i = ig_low,ig_high
-!    write(*,100) grid%xc(i), grid%Ac(i), soln%mach(i), soln%V(i,1), soln%V(i,2), soln%V(i,3)/1000.0_prec
-!  end do
-!  write(*,*)
+  call solve_exact_q1d( ex_soln, grid)
   
   call sub_in_bndry( soln%mach, soln%U, soln%V )
 
@@ -66,28 +50,29 @@ program main_program
   call cons2prim( soln%U, soln%V )
  
   call speed_of_sound(soln%V(:,3),soln%V(:,1),soln%asnd)
+  
   call calculate_sources(soln%V,grid%dAc,soln%src)
+  
   call calc_time_step(grid%dx,soln%V,soln%asnd,soln%lambda,soln%dt)
-!  write(*,*) 'Calculate Sources and Time step:'
-!  write(*,*) trim(adjustl(header_str4))
-!  do i = ig_low,ig_high
-!    if( (i>=i_low).and.(i<=i_high) ) then
-!      write(*,300) grid%xc(i),grid%dAc(i), soln%src(i), soln%dt(i), soln%lambda(i)
-!    else 
-!      write(*,300) grid%xc(i), zero, zero, zero, soln%lambda(i)
-!    end if
-!  end do
-!  write(*,*)
-  !call prim2cons(soln%U,soln%V)
+  
   call central_flux( soln%U, soln%F )
-
-    call prim2cons(soln%U,soln%V)
-
+  
+  call prim2cons(soln%U,soln%V)
+  
   call jst_damping(soln%lambda,soln%U,soln%V,soln%D) 
-    soln%F = soln%F + soln%D
-    call explicit_euler(grid,soln%src,soln%dt,soln%F,soln%U,soln%R)
-
-  do j = 1,800
+  
+  soln%F = soln%F + soln%D
+  
+  call explicit_euler(grid,soln%src,soln%dt,soln%F,soln%U,soln%R)
+  
+  call residual_norms(soln%R,soln%rinit,2,(/one,one,one/))
+  
+  write(*,*) 'Residual Norms: Iteration 0'
+  write(*,*) header_str1
+  write(*,100) j, soln%rinit(1,1), soln%rinit(1,2), soln%rinit(1,3)
+  write(*,*) 'Relative Residual Norms:'
+  
+  do j = 1,max_iter
     
     call calculate_sources(soln%V(:,3),grid%dAc,soln%src)
     
@@ -115,13 +100,56 @@ program main_program
     
     call cons2prim(soln%U,soln%V)
     
-    if (mod(j,4)==0) then
+    if (mod(j,1000)==0) then
       call output_soln(grid,soln,j)
     end if
-  
+    
+    call residual_norms(soln%R(:,:),rnorm(:,:),0,soln%rinit(:,:))
+    !soln%rnorm(j,1:neq) = rnorm(1,1:neq)
+    if (all(rnorm<tol) ) then
+      exit
+    end if
+    if (mod(j,1000)==0) then
+      write(*,*) header_str1
+    end if
+    if (mod(j,100)==0) then
+      write(*,100) j, rnorm(1,1), rnorm(1,2), rnorm(1,3)
+    end if
   end do
+  call output_soln(grid,soln,j+1)
   
-  !call deallocate_exact_q1d( ex_soln )
+  call deallocate_exact_q1d( ex_soln )
   call teardown_geometry(grid,soln)
 
 end program main_program
+
+!  100 format(2(F9.4),4(G20.7))
+!  200 format(1(F9.4),3(G20.7))
+!  300 format(2(F9.4),3(G20.7))
+!  write(header_str1,*) '|    x   |    A    |         M         |'// &
+!  &  '        rho        |         u        |         p        |'
+!  write(header_str2,*) '|    x   |        U(1)       |'// &
+!  & '       U(2)       |       U(3)       |'
+!  write(header_str3,*) '|    x   |        F(1)       |'// &
+!  & '       F(2)       |       F(3)       |'
+!  write(header_str4,*) '|    x   |    dA   |         S         |'// &
+!  & '         dt       |       lambda     |' 
+!  write(header_str5,*) '|    x   |        D(1)       |'// &
+!  & '       D(2)       |       D(3)       |'
+
+!  write(*,*) 'Initial Conditions: Primitives'
+!  write(*,*) trim(adjustl(header_str1))
+!  do i = ig_low,ig_high
+!    write(*,100) grid%xc(i), grid%Ac(i), soln%mach(i), soln%V(i,1), soln%V(i,2), soln%V(i,3)/1000.0_prec
+!  end do
+!  write(*,*)
+!  write(*,*) 'Calculate Sources and Time step:'
+!  write(*,*) trim(adjustl(header_str4))
+!  do i = ig_low,ig_high
+!    if( (i>=i_low).and.(i<=i_high) ) then
+!      write(*,300) grid%xc(i),grid%dAc(i), soln%src(i), soln%dt(i), soln%lambda(i)
+!    else 
+!      write(*,300) grid%xc(i), zero, zero, zero, soln%lambda(i)
+!    end if
+!  end do
+!  write(*,*)
