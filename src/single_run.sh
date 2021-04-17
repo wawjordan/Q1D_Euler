fdb41cb82ee016f0ea30b9f9a1137ed0e6121a2c
Will Jordan <wajordan@vt.edu>
2021-04-17 13:44:43 -0400

    added additional parameters

--- src/single_run.sh
+++ src/single_run.sh
@@ -12,17 +12,21 @@ cfl_str=""
 imax=128 #16 32 64 128 256 512
 p0=300.0
 T0=600.0
+prat=0.4 #0.1 0.5 0.6
 flux=2
+limiter=2
+beta_lim=2.0
 shock=0 #0 1
 ramp=0 #0 1
-prat=0.4 #0.1 0.5 0.6
-cfl=0.01 #0.1 0.5 0.9
+cfl=0.1 #0.1 0.5 0.9
+eps_roe=0.05
+eps_MUSCL=1.0
+kappa_MUSCL=-1.0
+k2=0.5 #0.5 0.4 0.3 0.25
+k4=0.03125 #0.03125 0.02 0.015625 
 maxk=100000
 Sout=100000
 Rout=100
-eps=0.05
-k2=0.5 #0.5 0.4 0.3 0.25
-k4=0.03125 #0.03125 0.02 0.015625 
 if [ $shock -eq 0 ]; then
   shock_str="Isentropic"
 else
@@ -42,17 +46,21 @@ echo "----|----|----|----|----|----|" >> $input
 echo "imax      $imax" >> $input
 echo "p0        $p0" >> $input
 echo "T0        $T0" >> $input
+echo "p_rat     $prat" >> $input
 echo "flux      $flux" >> $input
+echo "limiter   $limiter" >> $input
+echo "beta_L    $beta_lim" >> $input
 echo "shock     $shock" >> $input
 echo "ramp      $ramp" >> $input
-echo "p_rat     $prat" >> $input
 echo "CFL       $cfl" >> $input
+echo "eps_roe   $eps_roe" >> $input
+echo "eps_M     $eps_MUSCL" >> $input
+echo "k_M       $kappa_MUSCL" >> $input
+echo "k2        $k2" >> $input
+echo "k4        $k4" >> $input
 echo "maxk      $maxk" >> $input
 echo "Sout      $Sout" >> $input
 echo "Rout      $Rout" >> $input
-echo "eps       $eps" >> $input
-echo "k2        $k2" >> $input
-echo "k4        $k4" >> $input
 ./../build/bin/test_program
 #cat "$temp" >> $summary
 rm -f temp.txt
