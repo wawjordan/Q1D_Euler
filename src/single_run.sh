7dda5364e38115c1fccc5e9d8ec92e1f67cb5a17
Will Jordan <wajordan@vt.edu>
2021-04-18 22:52:55 -0400

    updated to output input.nml

--- src/single_run.sh
+++ src/single_run.sh
@@ -1,15 +1,16 @@
 #!./bin/bash
-input="input.dat"
+input="input.nml"
 summary="../results/summary.dat"
 temp="temp.txt"
 start=`date +%s`
 shock_str=""
-cfl_str=""
+flux_str=""
+limiter_str=""
 #touch $summary
 #if [ -f $summary ]; then
 #  rm -f "$summary"
 #fi
-imax=128 #16 32 64 128 256 512
+imax=16 #16 32 64 128 256 512
 p0=300.0
 T0=600.0
 prat=0.4 #0.1 0.5 0.6
@@ -17,50 +18,91 @@ flux=2
 limiter=2
 beta_lim=2.0
 shock=1 #0 1
-ramp=0 #0 1
-cfl=0.1 #0.1 0.5 0.9
+cfl=0.7 #0.1 0.5 0.9
 eps_roe=0.05
 eps_MUSCL=1.0
 kappa_MUSCL=-1.0
 k2=0.5 #0.5 0.4 0.3 0.25
 k4=0.03125 #0.03125 0.02 0.015625 
-maxk=100000
+maxk=150000
 Sout=10000
 Rout=100
+disp_out=100
 if [ $shock -eq 0 ]; then
-  shock_str="Isentropic"
+  shock_str="isentropic"
 else
-  shock_str="Normal Shock"
+  shock_str="normal shock"
 fi
-if [ $ramp -eq 1 ]; then
-  cfl_str="cfl-ramp"
-else
-  cfl_str="cfl-const"
+if [ $flux -eq 1 ]; then
+  flux_str="central scheme"
+elif [ $flux -eq 2 ]; then
+  flux_str="van Leer flux"
+elif [ $flux -eq 3 ]; then
+  flux_str="Roe's flux"
+fi
+
+if [ $flux -ne 1 ]; then
+if [ $limiter -eq 1 ]; then
+  limiter_str="van Leer limiter"
+elif [ $limiter -eq 2 ]; then
+  limiter_str="van Albada limiter"
+elif [ $limiter -eq 3 ]; then
+  limiter_str="minmod limiter"
+elif [ $limiter -eq 4 ]; then
+  limiter_str="beta ($beta_lim) limiter"
+fi
 fi
 echo ""
-echo "################################################################################"
-echo " N=$imax | $shock_str | $cfl_str | P_rat=$prat | k2=$k2 | k4=$k4 | CFL=$cfl "
-echo "################################################################################"
-echo "! Input Data" > $input
-echo "----|----|----|----|----|----|" >> $input
-echo "imax      $imax" >> $input
-echo "p0        $p0" >> $input
-echo "T0        $T0" >> $input
-echo "p_rat     $prat" >> $input
-echo "flux      $flux" >> $input
-echo "limiter   $limiter" >> $input
-echo "beta_L    $beta_lim" >> $input
-echo "shock     $shock" >> $input
-echo "ramp      $ramp" >> $input
-echo "CFL       $cfl" >> $input
-echo "eps_roe   $eps_roe" >> $input
-echo "eps_M     $eps_MUSCL" >> $input
-echo "k_M       $kappa_MUSCL" >> $input
-echo "k2        $k2" >> $input
-echo "k4        $k4" >> $input
-echo "maxk      $maxk" >> $input
-echo "Sout      $Sout" >> $input
-echo "Rout      $Rout" >> $input
+echo "!==============================================================================!"
+echo "| N=$imax | $shock_str | P_rat=$prat "
+echo "--------------------------------------------------------------------------------"
+echo "| $flux_str | $limiter_str | CFL=$cfl "
+echo "!==============================================================================!"
+
+echo "&grid" > $input
+echo "  imax = $imax" >> $input
+echo "  xmin = -1.0" >> $input
+echo "  xmax =  1.0" >> $input
+echo "  n_ghost_cells = 2" >> $input
+echo "/" >> $input
+echo "" >> $input
+echo "&geometry" >> $input
+echo "  Astar = 0.2" >> $input
+echo "/" >> $input
+echo "" >> $input
+echo "&constants" >> $input
+echo "  gamma = 1.4" >> $input
+echo "/" >> $input
+echo "" >> $input
+echo "&initial" >> $input
+echo "  p0 = $p0" >> $input
+echo "  T0 = $T0" >> $input
+echo "  p_ratio = $prat" >> $input
+echo "  shock = $shock" >> $input
+echo "/" >> $input
+echo "" >> $input
+echo "&numerical" >> $input
+echo "  CFL = $cfl" >> $input
+echo "  max_iter = $maxk" >> $input
+echo "/" >> $input
+echo "" >> $input
+echo "&flux" >> $input
+echo "  flux_scheme = $flux" >> $input
+echo "  limiter_scheme = $limiter" >> $input
+echo "  beta_lim = $beta_lim" >> $input
+echo "/" >> $input
+echo "" >> $input
+echo "&output" >> $input
+echo "  soln_save = $Sout" >> $input
+echo "  res_save = $Rout" >> $input
+echo "  res_out  = $disp_out" >> $input
+echo "/" >> $input
+echo "" >> $input
+echo "&reconstruction" >> $input
+echo "  epsM = $eps_MUSCL" >> $input
+echo "  kappaM = $kappa_MUSCL" >> $input
+echo "/" >> $input
+
 ./../build/bin/test_program
 #cat "$temp" >> $summary
 rm -f temp.txt
