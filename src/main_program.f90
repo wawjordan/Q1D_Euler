2ca508175e60df429728aebeb961da09c2a975c1
Will Jordan <wajordan@vt.edu>
2021-04-18 22:50:47 -0400

    added additional control over output display

--- src/main_program.f90
+++ src/main_program.f90
@@ -1,17 +1,15 @@
 program main_program
   
-  use set_precision, only : prec
   use set_constants, only : set_derived_constants
   use fluid_constants, only : set_fluid_constants
-  use set_inputs, only : set_derived_inputs, read_in
-  use set_inputs, only : max_iter, neq, tol, soln_save, res_save
+  use set_inputs, only : set_derived_inputs
+  use set_inputs, only : max_iter, tol, soln_save, res_save
   use set_inputs, only : leftV, rightV, leftU, rightU, flux_scheme
-  use set_inputs, only : limiter_freeze, psi_plus, psi_minus
+  use set_inputs, only : limiter_freeze, res_out
   use variable_conversion
   use time_integration
   use basic_boundaries, only : enforce_bndry
   use limiter_calc, only : select_limiter
-  !use fluxes
   use flux_calc, only : select_flux, flux_fun
   use other_subroutines
   use geometry, only : setup_geometry, teardown_geometry
@@ -23,17 +21,14 @@ program main_program
   implicit none
   
   character(len=100) :: header_str1
-  integer :: j, k, pnorm
-  !real(prec), dimension(3) :: rnorm
-  !real(prec), dimension(3) :: rinit
-  !real(prec), dimension(3) :: DEnorm
+  character(len=100) :: niter
+  integer :: j, pnorm
   type( grid_t )      :: grid
   type( soln_t )      :: soln
   type( exact_q1d_t ) :: ex_soln  
-  pnorm = 2
+  pnorm = 1
   j = 0
-  k = 0
-
+  
   open(50,file='temp.txt',status='unknown')
   
   100 format(1(I0.8),3(G20.7))
@@ -42,8 +37,7 @@ program main_program
   & "   ||velocity||    |   ||pressure||    |"
   call set_derived_constants
   call set_fluid_constants
-  call read_namelist
-  !call read_in
+  call read_namelist('input.nml')
   call output_file_headers
   call set_derived_inputs
   call setup_geometry(grid,soln)
@@ -67,11 +61,6 @@ program main_program
   call calculate_sources(soln%V(:,3),grid%dAc,soln%src)
   call calc_time_step(grid%dx,soln%V,soln%asnd,soln%lambda,soln%dt) 
   
-  !call MUSCL_extrap( soln%V, leftV, rightV )
-  !call prim2cons(leftU,leftV)
-  !call prim2cons(rightU,rightV)
-  !call flux_fun(leftU,rightU,soln%F)
-  
   if (flux_scheme==1) then
     call flux_fun(soln%U(i_low-1:i_high,:),soln%U(i_low:i_high+1,:),soln%F)
     call jst_damping(soln%lambda,soln%U,soln%V,soln%D)
@@ -82,11 +71,7 @@ program main_program
     call prim2cons(rightU,rightV)
     call flux_fun(leftU,rightU,soln%F)
   end if
-  !call flux_fun(soln%U(i_low-1:i_high,1:neq),soln%U(i_low:i_high+1,1:neq),soln%F)
-  !call flux_fun(leftU(i_low-1:i_high,1:neq),rightU(i_low-1:i_high,1:neq),soln%F)
-  !call jst_damping(soln%lambda,soln%U,soln%V,soln%D) 
   
-  !soln%F = soln%F + soln%D
   call explicit_euler(grid,soln%src,soln%dt,soln%F,soln%U,soln%R)
   call update_states( soln )
   
@@ -116,15 +101,9 @@ program main_program
       call prim2cons(rightU,rightV)
       call flux_fun(leftU,rightU,soln%F)
     end if
-    !call flux_fun(soln%U(i_low-1:i_high,1:neq),soln%U(i_low:i_high+1,1:neq),soln%F)
-    !call prim2cons(soln%U,soln%V)
-    
     
     call explicit_euler(grid,soln%src,soln%dt,soln%F,soln%U,soln%R)
     call update_states( soln )
-    !call update_mach(soln%V,soln%mach )
-    
-    !call cons2prim(soln%U,soln%V)
     
     if (mod(j,soln_save)==0) then
       if (shock.eq.0) then
@@ -137,21 +116,14 @@ program main_program
     
     if (all(soln%rnorm<tol) ) then
       exit
-    !elseif (mod(k,10000)==0) then
-    !  limiter_freeze = .false.
-    !  k = 1
     elseif (any(soln%rnorm>soln%rold) ) then
       limiter_freeze = .true.
     end if
     soln%rold = soln%rnorm
-    !k = k+1
     if (mod(j,10000)==0) then
       write(*,*) header_str1
     end if
-    if (mod(j,1000)==0) then
-      if (shock.eq.0) then
-        call calc_de( soln, ex_soln, soln%DE, soln%DEnorm, pnorm )
-      end if
+    if (mod(j,res_out)==0) then
       write(*,100) j, soln%rnorm(1), soln%rnorm(2), soln%rnorm(3)
     end if
     if (mod(j,res_save)==0) then
@@ -162,6 +134,16 @@ program main_program
     end if
   end do
   
+  if (mod(j,res_out)/=0) then
+    write(*,100) j, soln%rnorm(1), soln%rnorm(2), soln%rnorm(3)
+  end if
+  if (all(soln%rnorm<tol) ) then
+    write(*,*) 'Solution converged!'
+  else
+    write(niter,'(I7)') max_iter
+    write(*,*) 'Solution failed to converge in ',&
+       & trim(adjustl(niter)),' iterations'
+  end if
   write(50,*) imax, j, soln%rnorm(1), soln%rnorm(2), soln%rnorm(3)
   if (shock.eq.0) then 
     call calc_de( soln, ex_soln, soln%DE, soln%DEnorm, 1 )
